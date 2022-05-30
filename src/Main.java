b423da363d094e45169e74cc07e3992432bac9c1
Johannmk1 <104039187+Johannmk1@users.noreply.github.com>
2022-05-30 19:43:48 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		
+		System.out.println(" ");
+		System.out.println("====PARABÉNS====");
+		System.out.println("Nivel concluido");
+	}
+
+}
